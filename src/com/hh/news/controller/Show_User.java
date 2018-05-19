1b41c7414576cc833f1f2df323660c3f724d4b31
tryhardtoflybird <1243907764@qq.com>
2018-05-19 17:48:01 +0800

    login

--- src/com/hh/news/controller/Show_User.java
+++ src/com/hh/news/controller/Show_User.java
@@ -3,12 +3,17 @@ package com.hh.news.controller;
 import com.hh.news.entity.User;
 import com.hh.news.service.Show_UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 //用户模块
+@Controller
 public class Show_User {
 
     @Autowired
@@ -20,12 +25,21 @@ public class Show_User {
      * @return
      */
     @RequestMapping(value = "userLogin")
-    public String userLogin(User user, HttpServletRequest request){
-
+    public void userLogin(User user, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
         User existUser = show_userService.getUser(user);
-        HttpSession session = request.getSession();
-        session.setAttribute("user",existUser);
-        return "forward:/index.html";
+
+        String username = existUser.getUsername();
+
+        response.sendRedirect("index.html?user="+username);
+
+        }catch (Exception e){
+            String message = e.getMessage();
+            request.setAttribute("message",message);
+            request.getRequestDispatcher("/login.html").forward(request,response);
+
+        }
+
     }
 
 }
