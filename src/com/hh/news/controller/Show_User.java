00cddd4abff2b4094404f9d530747f90197ce45b
tryhardtoflybird <1243907764@qq.com>
2018-05-20 17:18:25 +0800

    login

--- src/com/hh/news/controller/Show_User.java
+++ src/com/hh/news/controller/Show_User.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 //用户模块
